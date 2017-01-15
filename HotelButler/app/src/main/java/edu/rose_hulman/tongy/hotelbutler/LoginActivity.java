8e77f09ce9efd74cc6cfd6bfc4f808717a82cfd0
Neo Tong <tongy@rose-hulman.edu>
2017-01-15 00:04:37 -0500

    Added Homepage activity
    Homepage activity and layout for it, includes buttons and image. Buttons
    logic not implemented yet.

--- HotelButler/app/src/main/java/edu/rose_hulman/tongy/hotelbutler/LoginActivity.java
+++ HotelButler/app/src/main/java/edu/rose_hulman/tongy/hotelbutler/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.rose_hulman.tongy.hotelbutler;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -187,6 +188,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent mainIntent =new Intent(this,MainActivity.class);
+            startActivity(mainIntent);
         }
     }
 
